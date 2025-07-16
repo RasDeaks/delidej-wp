c6eb8b8576748db9fbc223e8f95af2fb15216212
dix <francois.dixmeras@lunatech.fr>
2025-07-16 12:14:30 +0200

    modify comment to trigger deploy

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The base configuration for WordPress
+ * The BASE configuration for WordPress
  *
  * The wp-config.php creation script uses this file during the installation.
  * You don't have to use the website, you can copy this file to "wp-config.php"
